a74c0b6113693b6dbb82d69f9e4117f1b8e80ae8
Mustafa Hakan Borazan <borazanmh@outlook.com>
2023-09-09 05:28:21 +0300

    server listening log message fix

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use("/api/monsters", monsterRouter);
 app.use("/api/locations", locationRouter);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is listening on port", process.env.PORT);
+  console.log("Server is listening on port", process.env.PORT || 3000);
 });
